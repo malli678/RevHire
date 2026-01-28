593917ed5077675323683161e8cb151131c035d2
malli678 <mandavamalli94@gmail.com>
2026-01-28 10:34:02 +0530

    Day 1: Project setup and initial console entry point

--- src/com/revhire/RevHireApp.java
+++ src/com/revhire/RevHireApp.java
@@ -2,25 +2,34 @@ package com.revhire;
 
 import com.revhire.ui.MainMenu;
 import com.revhire.util.DBUtil;
+import com.revhire.util.LoggerUtil;
+import org.apache.log4j.Logger;
 
 public class RevHireApp {
+
+    private static final Logger logger = LoggerUtil.getLogger(RevHireApp.class);
+
     public static void main(String[] args) {
+
+        logger.info("Initializing RevHire Job Portal...");
         System.out.println("Initializing RevHire Job Portal...");
-        
+
         try {
             // Test database connection
             DBUtil.getConnection();
+            logger.info("Database connection successful!");
             System.out.println("Database connection successful!");
-            
+
             // Start the application
             MainMenu mainMenu = new MainMenu();
             mainMenu.show();
-            
+
         } catch (Exception e) {
+            logger.error("Failed to start RevHire", e);
             System.out.println("Failed to start RevHire: " + e.getMessage());
-            e.printStackTrace();
         } finally {
             DBUtil.closeConnection();
+            logger.info("Application shutdown");
             System.out.println("\nThank you for using RevHire!");
         }
     }
